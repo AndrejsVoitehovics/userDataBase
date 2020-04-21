7babefc35611c44d37052e78c91d25b07291286b
Andrej <Dumus@inbox.lv>
2020-04-21 15:25:04 +0300

    add Provider

--- app/src/main/java/com/example/clubolympus/data/ClubOlympusContract.java
+++ app/src/main/java/com/example/clubolympus/data/ClubOlympusContract.java
@@ -1,10 +1,18 @@
 package com.example.clubolympus.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class ClubOlympusContract {
     private ClubOlympusContract() {
     }
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "olympus";
+    public static final String SCHEME = "content://";
+    public static final String AUTHORITY = "com.example.clubolympus";
+    public static final String PATH_MEMBERS = "members";
+    public static final Uri BASE_CONTENT_URI = Uri.parse(SCHEME + AUTHORITY);
 
     public static final class MemberEntry implements BaseColumns {
         public static final String TABLE_NAME = "members";
@@ -13,7 +21,7 @@ public final class ClubOlympusContract {
         public static final String COLUMN_FIRSTNAME = "firstName";
         public static final String COLUMN_LASTNAME = "lastName";
         public static final String COLUMN_GENDER = "gender";
-        public static final String COLUMN_GROUP = "group";
+        public static final String COLUMN_SPORT = "sport";
 
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
